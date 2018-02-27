4858ab62f99441b2638abaedaf087a3561406968
unagiHuman <tigerwoods1206@gmail.com>
2018-02-27 19:09:07 +0900

    first commi

---
+++ Assets/System/CustomCoroutineTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLib.System;
+
+public class CustomCoroutineTest : MonoBehaviour {
+
+
+    IEnumerator TestCorutine()
+    {
+        while (true)
+        {
+           
+            yield return Nest1Coroutine();
+            Debug.Log("DoCoroutone");
+        }
+    }
+
+    IEnumerator Nest1Coroutine()
+    {
+        var time = 0f;
+        while (true)
+        {
+            if (time > 2f)
+            {
+                break;
+            }
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("Do Nest1 Coroutone " + time.ToString());
+            time += 0.5f;
+        }
+        yield return Nest2Coroutine();
+    }
+
+    IEnumerator Nest2Coroutine()
+    {
+        var time = 0f;
+        while (true)
+        {
+            if(time > 2f)
+            {
+                break;
+            }
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("Do Nest2 Coroutone " + time.ToString());
+            time += 0.5f;
+        }
+
+        yield return null;
+       
+    }
+
+    /*
+    [Button("DoCoroutine", "DoCoroutine", null)]
+    [SerializeField]
+    bool DoCoroutineButton;
+
+
+    CoroutineTask _task;
+
+    private void DoCoroutine()
+    {
+        _task = this.StartCoroutineTask(TestCorutine());
+        _task.StartCoroutine();
+    }
+
+
+    [Button("PauseCoroutine", "PauseCoroutine", null)]
+    [SerializeField]
+    bool PauseCoroutineButton;
+
+    private void PauseCoroutine()
+    {
+        _task.Pause();
+    }
+
+
+    [Button("ResumeCoroutine", "ResumeCoroutine", null)]
+    [SerializeField]
+    bool ResumeCoroutineButton;
+
+    private void ResumeCoroutine()
+    {
+        _task.Resume();
+    }
+
+    [Button("KillCoroutine", "KillCoroutine", null)]
+    [SerializeField]
+    bool KillCoroutineButton;
+
+    private void KillCoroutine()
+    {
+        _task.Kill();
+    }
+    */
+}
