5100970b92dd365a2ec4e781fe6b8d3eef3a696f
unagiHuman <tigerwoods1206@gmail.com>
2018-03-01 19:38:52 +0900

    [add] monobehaviourに紐づいたkill機能

--- Assets/GameLib/System/CoroutineTaskScheduler.cs
+++ Assets/GameLib/System/CoroutineTaskScheduler.cs
@@ -18,6 +18,9 @@ namespace GameLib.System
         CoroutineTask[] _coroutines = new CoroutineTask[InitialSize];
 
 
+        Dictionary<string, List<int>> _idDict = new Dictionary<string, List<int>>();
+
+
         private void Awake()
         {
             Instance = this;
@@ -27,28 +30,37 @@ namespace GameLib.System
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="mo"></param>
         /// <param name="enumerator"></param>
         /// <returns></returns>
-        public int StartCoroutineTask(IEnumerator enumerator)
+        public int StartCoroutineTask(MonoBehaviour mo, IEnumerator enumerator)
         {
-           
-            if(_coroutines.Length  == _fillsize)
+
+            if (_coroutines.Length == _fillsize)
             {
                 Array.Resize(ref _coroutines, checked(_fillsize * 2));
             }
 
-            int returnid=0;
+            int returnid = 0;
             var isAssign = false;
             _fillsize = 0;
             for (int i = 0; i < _coroutines.Length; i++)
             {
-                
+
                 if (_coroutines[i] == null && !isAssign)
                 {
-                    _coroutines[i] = new CoroutineTask(this, enumerator, _coroutineId);
+                    _coroutines[i] = new CoroutineTask(mo, enumerator, _coroutineId);
                     returnid = _coroutineId;
                     _coroutineId = (_coroutineId + 1) & 0x7FFFFFFF;
                     _coroutines[i].finishCoroutineCallback = FinishCallBack;
+
+                    if (!_idDict.ContainsKey(mo.name))
+                    {
+                        _idDict[mo.name] = new List<int>();
+                    }
+
+                    _idDict[mo.name].Add(returnid);
+
                     _coroutines[i].StartCoroutine();
                     isAssign = true;
                 }
@@ -69,14 +81,42 @@ namespace GameLib.System
         /// <param name="id"></param>
         public void Kill(int id)
         {
-            for(int index = 0; index < _coroutines.Length; index++)
+            for (int index = 0; index < _coroutines.Length; index++)
             {
-                if(_coroutines[index] != null && _coroutines[index].id == id)
+                if (_coroutines[index] != null && _coroutines[index].id == id)
                 {
                     _coroutines[index].Kill();
                     break;
                 }
             }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mo"></param>
+        public void Kill(MonoBehaviour mo)
+        {
+
+            for (int index = 0; index < _coroutines.Length; index++)
+            {
+                if (!_idDict.ContainsKey(mo.name)) continue;
+                foreach (var id in _idDict[mo.name])
+                {
+                    if (_coroutines[index] != null && _coroutines[index].id == id)
+                    {
+                        _coroutines[index].Kill();
+                        break;
+                    }
+                }
+            }
+            if (_idDict.ContainsKey(mo.name))
+            {
+                _idDict[mo.name].Clear();
+                _idDict.Remove(mo.name);
+            }
+
         }
 
         public void KillAll()
@@ -86,8 +126,12 @@ namespace GameLib.System
                 if (_coroutines[index] != null)
                 {
                     _coroutines[index].Kill();
-                  
                 }
+            }
+
+            foreach (var one in _idDict)
+            {
+                _idDict.Remove(one.Key);
             }
         }
 
@@ -100,6 +144,21 @@ namespace GameLib.System
                 {
                     _coroutines[index].Pause();
                     break;
+                }
+            }
+        }
+
+        public void Pause(MonoBehaviour mo)
+        {
+            for (int index = 0; index < _coroutines.Length; index++)
+            {
+                foreach (var id in _idDict[mo.name])
+                {
+                    if (_coroutines[index] != null && _coroutines[index].id == id)
+                    {
+                        _coroutines[index].Pause();
+                        break;
+                    }
                 }
             }
         }
@@ -130,6 +189,22 @@ namespace GameLib.System
         }
 
 
+        public void Resume(MonoBehaviour mo)
+        {
+            for (int index = 0; index < _coroutines.Length; index++)
+            {
+                foreach (var id in _idDict[mo.name])
+                {
+                    if (_coroutines[index] != null && _coroutines[index].id == id)
+                    {
+                        _coroutines[index].Resume();
+                        break;
+                    }
+                }
+            }
+        }
+
+
         public void ResumeAll()
         {
             for (int index = 0; index < _coroutines.Length; index++)
@@ -142,11 +217,16 @@ namespace GameLib.System
         }
 
 
-        private void FinishCallBack(int coroutineId)
+        private void FinishCallBack(MonoBehaviour mo, int coroutineId)
         {
-            for(int index = 0; index < _coroutines.Length; index++)
+            if (_idDict.ContainsKey(mo.name))
             {
-                if(_coroutines[index] != null && _coroutines[index].id == coroutineId)
+                _idDict[mo.name].Remove(coroutineId);
+            }
+
+            for (int index = 0; index < _coroutines.Length; index++)
+            {
+                if (_coroutines[index] != null && _coroutines[index].id == coroutineId)
                 {
                     _coroutines[index] = null;
                 }
